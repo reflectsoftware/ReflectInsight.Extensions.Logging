95197d9dd3ba5dcbab12977f26100b90d3e54805
codematrix <ross_pellegrino@hotmail.com>
2018-05-31 19:58:55 -0400

    Intercepting EF Core commands and displaying them as SQL commands arguments.

--- src/ReflectInsight.Extensions.Logging/ReflectInsightLogger.cs
+++ src/ReflectInsight.Extensions.Logging/ReflectInsightLogger.cs
@@ -51,6 +51,12 @@ namespace ReflectInsight.Extensions.Logging
         {
             var formatter = (state as FormattedLogValues);
             var message = formatter?.ToString() ?? (state.ToString() ?? string.Empty);
+            
+            if(message.IndexOf("Microsoft.EntityFrameworkCore", StringComparison.OrdinalIgnoreCase) < 0)
+            {
+                return null;
+            }
+
             return GetLogger().TraceMethod(message);
         }
 
